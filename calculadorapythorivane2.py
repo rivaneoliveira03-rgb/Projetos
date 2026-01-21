76d463ecc8652f2f73e192c5ebe7534c869dc74b
Rivane <rivaneoliveira@hotmail.com>
2026-01-21 12:42:20 -0300

    alteração de cor

--- calculadorapythorivane2.py
+++ calculadorapythorivane2.py
@@ -5,12 +5,12 @@ from tkinter import *
 # cores
 
 cor1 ="#171715"  # preto
-cor2 ="#a5fc03"  # green/verde
+cor2 ="#FEFEFE"  # branco
 cor3 ="#033dfc"  # blue/azul
 cor4 ="#6f03fc"  # purple/roxo
-cor5 ="#fc0328"  # red/vermelho
-
-#TESTE
+cor5 ="#FC6E0F"  # laranja
+cor6 ="#BAAFAF"  # cinza
+cor7 ="#7C7878"  # cinza escuro 
 
 janela  =Tk()
 janela.title("Calculadora")
@@ -18,7 +18,7 @@ janela.geometry("235x318")
 janela.config(bg=cor1)
 
 # criando frames
-frame_tela = Frame(janela, width=235, height=50, bg=cor3)
+frame_tela = Frame(janela, width=235, height=50, bg=cor7)
 frame_tela.grid(row=0, column=0)
 
 frame_corpo = Frame(janela, width=235, height=268)
@@ -26,7 +26,13 @@ frame_corpo.grid(row=1, column=0)
 
 # criando botoes 
 
-b_1 = Button(frame_corpo, text="C", width=11, height=2)
+b_1 = Button(frame_corpo, text="C", width=11, height=2, bg=cor6)
 b_1.place(x=0, y=0)
+b_2 = Button(frame_corpo, text="%", width=5, height=2, bg=cor6)
+b_2.place(x=88, y=0)
+b_3 = Button(frame_corpo, text="/", width=5, height=2, bg=cor5, fg=cor2)
+b_3.place(x=177, y=0)
+
 
 janela.mainloop()
+
